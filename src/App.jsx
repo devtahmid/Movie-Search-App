fc1346311b0b44d045ff9198ba456ae3dd265504
devtahmiddfdfdf <shaikhtahmidurrahman1@gmail.com>
2023-01-24 22:13:01 +0300

    push to repo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SearchMovies from "./SearchMovies.jsx";
+
+export default function App() {
+
+  return (
+    <div className="container">
+      <h1 className="title">Movie Search App</h1>
+      <SearchMovies />
+    </div>
+  );
+
+}
+
